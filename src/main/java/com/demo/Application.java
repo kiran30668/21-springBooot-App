18c9d3c8376cc887148a931d19da64662f69b8fe
saikirankarurr <saikirankarurr@gmail.com>
2021-01-19 22:13:33 +0530

    HIS:300 Related Changes

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -6,11 +6,16 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
+	private Map<String,Object> cache=new HashMap<String,Object>();
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run=SpringApplication.run(Application.class, args);
 		//changes for HIS-134
 		run.close();
+	}
+
+	//HIS-300 changes
+	public void loadDataToCache(){
+		//Logic
 	}
 
 }
