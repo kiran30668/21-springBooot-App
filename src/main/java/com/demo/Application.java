94e973b2e6e57fd61bcc915306c3446252b442a5
saikirankarurr <saikirankarurr@gmail.com>
2021-01-19 22:05:47 +0530

    HIS-301:Related Changes

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
+	public static final String REST_URL="http://www.equitax.com/getScores";
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run=SpringApplication.run(Application.class, args);
 		//changes for HIS-134
